94272d544870ce5b336bd968ae6d762102bd10d4
charlene-johnson <charlene.j8234@gmail.com>
2020-08-21 02:31:54 -0400

    Created the database and did a get cars, it returns empty array as there is no data currently in the table

---
+++ cars/cars-router.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const db = require("../data/config")
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+    try {
+        res.json(await db("cars"))
+    } catch(err) {
+        next(err)
+    }
+})
+
+
+
+module.exports = router
